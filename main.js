5b2f1b3d53e851d89d2cb2ec4d698908f3b5072d
Marina8989 <marina.mill209@gmail.com>
2021-05-22 20:28:24 -0700

    working on js part

--- main.js
+++ main.js
@@ -4,17 +4,35 @@ const tags = document.querySelector('.tags');
 
 textarea.addEventListener('keyup', (event) => {
     getValue(event.target.value);
+    pressedEnter(event.key);
 })
 
 function getValue(input) {
     const text = input.split(',').filter(item => item.trim() !== '').map(item => item.trim());
     //console.log(text);
+    tags.innerText = '';
+
     text.forEach(tag => {
         console.log(tag)
-        // const span = document.createElement('span');
-        // span.classList.add('tag');
-        
+        const span = document.createElement('span');
+        span.classList.add('tag');
+        span.innerText = tag;
 
-        // tags.append(span);
+        tags.append(span);
     })
+
+     
 }
+//Know when enter is pressed
+//randomly pick a tag for sometime
+//pick for final iteration
+
+function pressedEnter(action){
+
+  setTimeout(() => {
+
+          console.log('lalala');
+  }, 100);
+}
+
+
